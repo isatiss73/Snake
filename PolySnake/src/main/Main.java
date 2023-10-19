4fee04f864ecb246987fdb1bae90e838deb8b656
qisat <qisat@Isatis-Ideapad>
2023-10-19 16:30:28 +0200

    intégration propre de javafx + app de base moche

--- PolySnake/src/main/Main.java
+++ PolySnake/src/main/Main.java
@@ -1,18 +1,47 @@
 package main;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 
 /**
  * main application class
  */
-public class Main
+public class Main extends Application
 {
 	/**
 	 * main application entry point
-	 * @param args pogram's arguments (nothing special here)
+	 * @param args program's arguments (nothing special here)
 	 */
 	public static void main(String[] args)
 	{
 		System.out.println("- = MAIN THREAD START = -");
+		launch(args);
 		System.out.println("- = MAIN THREAD END = -");
+	}
+	
+	@Override
+	public void start(Stage stage) throws Exception
+	{
+		stage.setTitle("PolySnake");
+		Button button = new Button();
+		button.setText("Play in terminal");
+		button.setOnAction(new EventHandler<ActionEvent>()
+		{
+			@Override
+			public void handle(ActionEvent event)
+			{
+				System.out.println("Play here bro");
+			}
+		});
+		StackPane root = new StackPane();
+		root.getChildren().add(button);
+		stage.setScene(new Scene(root, 300, 250));
+		stage.show();
 	}
 }
